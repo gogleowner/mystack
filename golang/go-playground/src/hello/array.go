b1578680659af4402e6d809e957aab02db750a68
gogleowner <gogleowner@gmail.com>
2019-11-14 00:05:29 +0900

    [golang] add array example

---
+++ golang/go-playground/src/hello/array.go
@@ -0,0 +1,25 @@
+package main
+
+import . "fmt"
+
+func main() {
+  // definition
+  var a = [5]int{32,29,78,16,81}
+
+  Println(a)
+
+  var b = [...]int {
+    1,
+    3,
+    5,
+    6,
+    9,
+  }
+
+  Println(b)
+
+  // iterate
+  for i := 0; i < len(a); i++ {
+    Println(a[i])
+  }
+}
